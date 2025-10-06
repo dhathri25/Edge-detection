6b3e66c70228619c2ef579bd64e56160d326cd9b
Dhathri Putty <143885665+dhathri25@users.noreply.github.com>
2025-10-06 18:58:02 +0530

    Create GLRender.java

---
+++ gl/GLRender.java
@@ -0,0 +1,64 @@
+package com.example.edgeviewer.gl;
+
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class GLRenderer implements GLSurfaceView.Renderer {
+    private FloatBuffer vertexBuffer;
+    private int textureId;
+    private byte[] imageData;
+
+    private final float[] vertices = {
+        -1.0f,  1.0f,
+        -1.0f, -1.0f,
+         1.0f,  1.0f,
+         1.0f, -1.0f
+    };
+
+    public GLRenderer() {
+        ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(vertices);
+        vertexBuffer.position(0);
+    }
+
+    public void updateImage(byte[] data) {
+        imageData = data;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        textureId = createTexture();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        GLES20.glViewport(0, 0, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        if (imageData != null) {
+            // Upload imageData to texture and render
+            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+            // Add texture binding and drawing logic here
+        }
+    }
+
+    private int createTexture() {
+        int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        return textures[0];
+    }
+}
